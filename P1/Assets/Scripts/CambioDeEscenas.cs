8a462f142ff960990b8f55a642aaa13dea09a4ee
Laboratorio.Diseño <lab.diseno@unisimon.edu.co>
2026-08-31 20:21:59 -0500

    Ground plane
    ...

---
+++ P1/Assets/Scripts/CambioDeEscenas.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioDeEscenas : MonoBehaviour
+{
+    public void CargarEscena(string que_escena)
+    {
+        SceneManager.LoadScene(que_escena);
+    }
+}
